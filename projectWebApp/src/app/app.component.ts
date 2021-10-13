803fefa98dc24454d90ac4eace628c7ee6e0e1e8
José Cardona <jose.eduardo.cardona@gmail.com>
2021-10-12 21:09:14 -0600

    nav var and icons

--- projectWebApp/src/app/app.component.ts
+++ projectWebApp/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,Renderer2, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'projectWebApp';
+
+
+  constructor(private renderer: Renderer2){
+
+  }
+
+  ngOnInit(){
+    const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li') as NodeListOf<HTMLElement>;
+      burger?.addEventListener('click', () => {
+        nav?.classList.toggle('nav-active');
+        burger?.classList.toggle('toggle');
+        navLinks?.forEach((link, index) => {
+          if(link.style.animation){
+            link.style.animation =''
+          }else{
+           link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
+         });
+
+          burger.classList.toggle('toggle');
+
+      });
+
+    }
+    navSlide();
+  
+  }
 }
--- projectWebApp/src/app/components/create/create.component.ts
+++ projectWebApp/src/app/components/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Global } from 'src/app/service/global';
 import { ProjectService } from 'src/app/service/project.service';
@@ -16,13 +16,15 @@ export class CreateComponent implements OnInit {
 public projects: any;
 public url: string;
 
-  constructor( private _projectService: ProjectService ) { 
+  constructor( private _projectService: ProjectService, private renderer: Renderer2, private http: HttpClient) {
+  
     this.url = Global.url;
   }
                
   ngOnInit() {
      this.seeAllProjects();
   }
+
 
   seeAllProjects(){
     this._projectService.getProjects().subscribe(
