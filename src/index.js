ac10f32fcb4676c27ab23f297c1fada1e8b0344e
José Cardona <jose.eduardo.cardona@gmail.com>
2021-11-11 22:29:06 -0600

    heroku port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 'use strict'
 const mongoose = require('mongoose');
 const app = require('../src/app.js');
-const port = 3000;
+const port = 3700;
 mongoose.promise = global.promise;
-//mongodb://localhost:27017/portfolio
 mongoose.connect('mongodb+srv://cardona:Umr3H2sjYw9dbWp@cluster0.7z3lp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(()=>{
     console.log("conexion establecida");
