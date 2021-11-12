1880173008cb53fcb5007a4d88f7aa425996e3e4
José Cardona <jose.eduardo.cardona@gmail.com>
2021-11-11 21:58:59 -0600

    mongodb server

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const app = require('../src/app.js');
 const port = 3000;
 mongoose.promise = global.promise;
-mongoose.connect('mongodb://localhost:27017/portfolio')
+//mongodb://localhost:27017/portfolio
+mongoose.connect('mongodb+srv://cardona:Umr3H2sjYw9dbWp@cluster0.7z3lp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(()=>{
     console.log("conexion establecida");
     app.listen(port,()=>{
