128c24146501195be504db5a2e690320b9e42251
José Cardona <jose.eduardo.cardona@gmail.com>
2021-11-11 22:34:54 -0600

    heroku port env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 'use strict'
 const mongoose = require('mongoose');
 const app = require('../src/app.js');
-const port = 3700;
 mongoose.promise = global.promise;
 mongoose.connect('mongodb+srv://cardona:Umr3H2sjYw9dbWp@cluster0.7z3lp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(()=>{
     console.log("conexion establecida");
-    app.listen(port,()=>{
+    app.listen((process.env.PORT || 3700),()=>{
         console.log("servidor corriendo correctamente");
     });
 })
