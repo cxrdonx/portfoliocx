615b142e3077b891b9d36f37aca55a7857d9714d
José Cardona <jose.eduardo.cardona@gmail.com>
2021-12-16 22:59:42 -0600

    refresh token updated

--- src/controllers/projectsController.js
+++ src/controllers/projectsController.js
@@ -12,7 +12,7 @@ var path = require('path');
 const CLIENT_ID = '993748911568-7c65j18ccf4n0jem6g5qi3gc1ee1ubqd.apps.googleusercontent.com';
 const CLIENT_SECRET ='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';
 const REDIRECT_URI ='https://developers.google.com/oauthplayground';    
-const REFRESH_TOKEN ='1//04_MRd_lBBPeJCgYIARAAGAQSNwF-L9IruKfNsfEhq9-wmJQdFqoP0fxvAD6fBab_9_uz0v5Wx0cBDBSt_FzJC2mM9S87ZvtzwFw';
+const REFRESH_TOKEN ='1//04Sd6F6RZmy8-CgYIARAAGAQSNwF-L9Ir5Lazlb5jHYs2QDDIsUtsNVWGh6rKmWw3KoFOoaTbvgZXyAqC19D0CTnUjstdUTWwoHw';
 const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 oauth2Client.setCredentials({
   refresh_token: REFRESH_TOKEN
